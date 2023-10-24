468e84ab4341ca461e5a93d25019d6228721da58
AndreaVelazco <104018328+AndreaVelazco@users.noreply.github.com>
2023-10-23 22:13:25 -0500

    (C) Inicializando la tabla hash

--- student_file/hashing.c
+++ student_file/hashing.c
@@ -17,3 +17,13 @@ int mi_Mod(int x, int n) {
 int randomFn(int x, int n) {
     return rand() % n;
 }
+
+void initHashTable(HashTable* ht, int (*hash_function)(int)) {
+    ht->size = TABLE_SIZE;
+    ht->table = (int*)malloc(TABLE_SIZE * sizeof(int));
+    ht->hash_function = hash_function;
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        ht->table[i] = -1;  
+    }
+}
