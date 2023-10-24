7aec2b52864c9a573360873c7fa1c91d5e9a1f0a
AndreaVelazco <104018328+AndreaVelazco@users.noreply.github.com>
2023-10-23 21:59:01 -0500

    Update hashing.py

--- student_file/hashing.py
+++ student_file/hashing.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import choice
 
-
 class HashTable:
     def __init__(self, hash_function, size):
         self.size = size
@@ -11,23 +10,24 @@ class HashTable:
         self.hash_function = hash_function
 
     def insert(self, x):
-        index = self.hash_function(x) % self.size
+        index = self.hash_function(x)
         if self.table[index] is None:
             self.table[index] = [x]
         else:
             self.table[index].append(x)
 
     def delete(self, x):
-        index = self.hash_function(x) % self.size
+        index = self.hash_function(x)
         if self.table[index] is not None and x in self.table[index]:
             self.table[index].remove(x)
 
     def find(self, x):
-        index = self.hash_function(x) % self.size
+        index = self.hash_function(x)
         if self.table[index] is not None and x in self.table[index]:
             return True
-        return False
- 
+        else:
+            return False
+
 def mi_Mod(x, n=10):
     return x % n
 
@@ -35,21 +35,43 @@ def randomFn(x, n=10):
     return choice(range(n))
 
 def RandomHashFun(M, n=10):
-        fnTable = [ None for i in range(M) ]
-        for x in range(M):
-            fnTable[x] = choice(range(n))
-        def randomFn(x):
-         return fnTable[x]
-        return randomFn
+    fnTable = [None] * M
+    for x in range(M):
+        fnTable[x] = choice(range(n))
+
+    def randomFnTake2(x):
+        return fnTable[x]
+
+    return randomFnTake2
 
 randomFn2 = RandomHashFun(1000, 10)
-print(randomFn2(52))
-print(randomFn2(324))
 
-HT = HashTable(randomFnTake2, 10)
+HT = HashTable(mi_Mod, 10)
+
+x = 1234567
+y = 76554334234
+HT.insert(x)
+
+print(HT.find(x))  # True
+print(HT.find(y))  # False
+
+HT.delete(x)
+print(HT.find(x))  # False
+
+HT = HashTable(randomFn, 10)
+
+x = 1234567
+y = 76554334234
+HT.insert(x)
+
+print(HT.find(x))  # True
+print(HT.find(y))  # False
+
+HT = HashTable(randomFn2, 1000)
 
 x = 123
 y = 76
 HT.insert(x)
 
-## verificar que la tabla hash funciona
+print(HT.find(x))  # True
+print(HT.find(y))  # False
