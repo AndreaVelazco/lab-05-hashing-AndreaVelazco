1563dff77c6c9ef5fa1f74558bcec1fa5d0aa5ac
AndreaVelazco <104018328+AndreaVelazco@users.noreply.github.com>
2023-10-23 22:03:58 -0500

    Estructura de la tabla hash

--- student_file/hashing.c
+++ student_file/hashing.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+#define TABLE_SIZE 10
+
+typedef struct HashTable {
+    int size;
+    int* table;
+    int (*hash_function)(int);
+} HashTable;
